f2e0cf9caf1692d59101da461f81e779cdc94268
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-11-07 00:25:23 +0100

    Server-side filter sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,7 +1,26 @@
+import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 
+
 export function useBookings() {
+    const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get("status");
+  // field param is the column name in the database - will be read in the apiBookings.js file
+  const filter = !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  // Sorting
+  // Here "startDate-desc" is the default value if no sortBy query param
+  // "startDate" is the column name in the database - will be read in the apiBookings.js file
+  // "desc" is the direction of the sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
 
   // QUERY
   const {
@@ -9,8 +28,8 @@ export function useBookings() {
     data: { data: bookings } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: () => getBookings(),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
