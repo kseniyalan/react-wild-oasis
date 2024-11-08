17f09320ee32b9989e46b64132aad197c4fb2b28
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-11-09 00:19:27 +0100

    Login form

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,21 +1,41 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
-// "as" prop is used to determine the heading level 
-// it is a part of styled-components library and used to say what HTML element to render
+// const test = css`
+//   text-align: center;
+//   ${10 > 5 && "background-color: yellow"}
+// `;
+
 const Heading = styled.h1`
-    ${ props => props.as === 'h1' && css`
-        font-size: 3rem;
-        font-weight: 600;
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
     `}
-    ${ props => props.as === 'h2' && css`
-        font-size: 2rem;
-        font-weight: 600;
+
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
     `}
-    ${ props => props.as === 'h3' && css`
-        font-size: 2rem;
-        font-weight: 500;
+    
+    ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
     `}
-    line-height: 1.4;
+    
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
+    `}
+    
+  line-height: 1.4;
 `;
 
 export default Heading;
