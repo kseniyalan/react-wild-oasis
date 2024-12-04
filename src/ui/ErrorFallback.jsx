86adc4dcbfca04574b1f393def0eefdda0e01705
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-12-04 22:36:54 +0100

    Use Error boundaries

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,24 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+// ErrorFallback component - is a fallback component for the ErrorBoundary component in main.jsx
+// It displays an error message and a button to try again
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 🧐</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
