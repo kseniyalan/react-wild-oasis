fa2dbe7d11585091e252e2ec1a76301c8d567332
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-12-04 20:48:41 +0100

    Dark mode completed

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,8 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { HiOutlineUser } from "react-icons/hi2";
 import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -18,6 +19,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
